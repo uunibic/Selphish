b2b39a5da90c37da11eaf236288729061eecac35
unibic <64989501+uunibic@users.noreply.github.com>
2021-09-26 19:12:44 +0530

    Add files via upload

---
+++ selphish/selphish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "\n\033[1m[+]\033[0m \e[40;38;5;82m SELPHISH \e[30;48;5;82m -By ACKO \e[0m"
+echo -e "\n                                              "
+
+site=$1
+
+cd scripts/
+
+python3 dmarc.py $site
+
+echo -e "\n\033[1m\e[95m-------------------------------------------------------\033[0m"
+echo -e "\n\033[1m\e[93m[+]\033[31m Taking ScreenShot of the URL.....\033[1m"
+
+./scan.sh $site >/dev/null 2>&1
+
+./vt.sh $site
